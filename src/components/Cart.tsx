7f43c5897614e8e90ec7d8eac22daccfb2d5a9a4
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-09-04 16:02:46 +0200

    Adds toggle to the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Button from './Button/Button'
 
 type Props = {}
 
 function Cart({ }: Props) {
+    const [responsiveState, setResponsiveState] = useState<Boolean>(true);
+
+    const cartView = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        setResponsiveState(true)
+    }
+
+    const orderButton = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        setResponsiveState(false)
+    }
+
     return (
         <>
-            <div className="fixed left-0 w-full lg:relative lg:w-80 lg:h-auto  bg-gray-200 p-5">
-                <h3 className='text-2xl text-center mr-auto lg:mr-0'>Winkelmandje</h3>
-                <ul className="lg:block cart-items mb-4">
+            <div className={`${responsiveState ? 'bottom-0' : 'h-full top-0'} fixed left-0 w-full lg:relative lg:w-80 lg:h-auto  bg-gray-200 p-5`}>
+                <div className={`${responsiveState ? 'hidden' : 'mb-5 block'} justify-center items-center flex lg:block`}>
+                    <h3 className='text-2xl text-center mr-auto lg:mr-0'>Winkelmandje</h3>
+                    <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden" onClick={(e) => cartView(e)}>
+                        <span className="sr-only">Close menu</span>
+                        <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+                <ul className={`${responsiveState ? 'hidden' : 'block'} lg:block mb-4`}>
                     <li className="py-4 border-b border-gray-300">
                         <div className="flex flex-col items-end">
                             <div className="w-full">
@@ -42,7 +64,7 @@ function Cart({ }: Props) {
                     </li>
                 </ul>
                 <p className='mb-2 font-bold'>Totaal: €</p>
-                <Button>Bestellen</Button>
+                <button className="px-2 py-1 bg-red-600 rounded text-white w-full disabled:opacity-50" onClick={(e) => orderButton(e)}> Bestellen</button>
             </div>
         </>
     )
