0ecb0c33d3e114875274875e37996033250c54eb
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-09-06 15:13:58 +0200

    Adds more icons

--- src/components/Restaurant.tsx
+++ src/components/Restaurant.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faBicycle } from '@fortawesome/free-solid-svg-icons'
 
 import { RestaurantType } from '@/types/Restaurant'
 
@@ -35,7 +36,7 @@ function Restaurant({ name, categories, image, reviews, delivery_time, delivery_
                 </figure>
                 <div className='p-5 bg-gray-100 grow group-hover:bg-gray-200 transition-colors rounded-r-md'>
                     <h3 className='font-bold mb-2 text-xl'>{name}</h3>
-                    <div className='flex mb-1'>
+                    <div className='flex mb-2'>
                         <div className='text-sm flex items-center font-bold mr-3'>
                             <div className='w-4 h-4 mr-2'>
                                 <FontAwesomeIcon icon={faStar} color="orange" className='flex-shrink-0' />
@@ -45,10 +46,20 @@ function Restaurant({ name, categories, image, reviews, delivery_time, delivery_
                         </div>
                         <p className='text-sm'>{categories.join(', ')}</p>
                     </div>
-                    <div>
+                    <div className='flex items-center'>
                         <span className='text-sm mr-3'>{delivery_time}</span>
-                        <span className='text-sm mr-3'>€ {delivery_cost}</span>
-                        <span className='text-sm'>€ {minimum_order_cost}</span>
+                        <span className='text-sm mr-4 inline-flex items-center'>
+                            <div className='w-4 h-4 mr-1 mt-0.5'>
+                                <FontAwesomeIcon icon={faBicycle} className='flex-shrink-0' />
+                            </div>
+                            <span>€ {delivery_cost}</span>
+                        </span>
+                        <span className='text-sm inline-flex items-center'>
+                            <div className='w-4 h-4 mr-1 mt-0.5'>
+                                <FontAwesomeIcon icon={faBicycle} className='flex-shrink-0' />
+                            </div>
+                            € {minimum_order_cost}
+                        </span>
                     </div>
                 </div>
             </div>
