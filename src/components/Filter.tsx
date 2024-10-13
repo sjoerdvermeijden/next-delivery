8c111c391a77a56aed632647bf26822e025c9569
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-10-13 16:54:01 +0200

    Ben er helaas nog niet uitgekomen

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -18,10 +18,12 @@ type Filter = {
 function Filter() {
     const [filterItems, setFilterItems] = useContext(FilterContext);
 
-    const addFilter = (e: React.MouseEvent<HTMLElement>, item: Filter) => {
-
-        if (filterItems.includes(item.title)) {
-            return false;
+    const toggleFilter = (e: React.MouseEvent<HTMLElement>, item: Filter) => {
+        const index = filterItems.indexOf(item.title);
+        if (index !== -1) {
+            let filterList = filterItems
+            filterList.splice(index, 1);
+            setFilterItems(filterList)
         } else {
             setFilterItems([...filterItems, item.title])
         }
@@ -32,9 +34,10 @@ function Filter() {
             <ul className='flex gap-10'>
                 {
                     filters?.map((item) => {
+
                         return (
-                            <li key={Math.random()} onClick={(e) => addFilter(e, item)} className='hover:cursor-pointer'>
-                                <div className='w-20 h-10 bg-red-500 rounded mb-1'></div>
+                            <li key={Math.random()} onClick={(e) => toggleFilter(e, item)} className='hover:cursor-pointer'>
+                                <div className={`w-20 h-10 ${filterItems.includes(item.title) ? 'bg-green-600' : 'bg-red-500'} rounded mb-1`}></div>
                                 <span className='text-sm'>{item?.title}</span>
                             </li>
                         )
--- src/components/Restaurants.tsx
+++ src/components/Restaurants.tsx
@@ -17,14 +17,6 @@ function Restaurants() {
     const [filterItems, setFilterItems] = useContext(FilterContext);
     const [restaurantItems, setRestaurantItems] = useState<RestaurantType[]>([]);
 
-    useEffect(() => {
-        const result = restaurants.filter((item) => item.categories.sort().toString().includes(filterItems.toString()));
-
-        console.log(result)
-
-
-    }, [filterItems])
-
     return (
         <>
             <Wrap>
@@ -32,7 +24,7 @@ function Restaurants() {
                     <h3 className='font-bold mb-6 text-2xl'>Bestel bij 4 locaties</h3>
                     <ul className='grid gap-5 items-start'>
                         {
-                            restaurants.map((item) => {
+                            restaurantItems.map((item) => {
                                 return <>
                                     <li key={item.id} className='group'>
                                         <Link href={`/restaurants/${item.id}`}>
