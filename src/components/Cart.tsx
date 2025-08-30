b198ad98135c5ec93f6cf8f36080707e27a75b7a
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-08-30 22:18:25 +0200

    Adds styling for the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -56,40 +56,42 @@ function Cart({ }: Props) {
     return (
         <>
             <div className={`${responsiveState ? 'bottom-0' : 'h-full top-0'} fixed bg-white left-0 w-full lg:relative lg:w-80 lg:h-auto  shadow-lg p-5 z-0`}>
-                <div className={`${responsiveState ? 'hidden' : 'mb-5 block'} justify-center items-center flex lg:block`}>
-                    <h3 className='text-2xl text-center mr-auto lg:mr-0'>Winkelmandje</h3>
-                    <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden" onClick={(e) => cartView(e)}>
-                        <span className="sr-only">Close menu</span>
-                        <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-                <ul className={`${responsiveState ? 'hidden' : 'block'} lg:block mb-4`}>
-                    {
-                        cartItems.map((item) => {
-                            return (
-                                <li className="py-4 border-b border-gray-300" key={item.id}>
-                                    <div className="flex flex-col items-end">
-                                        <div className="w-full">
-                                            <div className="flex">
-                                                <p className="mr-1">({item.count})</p>
-                                                <p className="mb-3 mr-auto">{item.title}</p>
-                                                <p className="font-bold">€{(item.price * item.count).toFixed(2)}</p>
+                <div className='sticky top-8 left-8'>
+                    <div className={`${responsiveState ? 'hidden' : 'mb-5 block'} justify-center items-center flex lg:block`}>
+                        <h3 className='text-2xl text-center mr-auto lg:mr-0'>Winkelmandje</h3>
+                        <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden" onClick={(e) => cartView(e)}>
+                            <span className="sr-only">Close menu</span>
+                            <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <ul className={`${responsiveState ? 'hidden' : 'block'} lg:block mb-4`}>
+                        {
+                            cartItems.map((item) => {
+                                return (
+                                    <li className="py-4 border-b border-gray-300" key={item.id}>
+                                        <div className="flex flex-col items-end">
+                                            <div className="w-full">
+                                                <div className="flex">
+                                                    <p className="mr-1">({item.count})</p>
+                                                    <p className="mb-3 mr-auto">{item.title}</p>
+                                                    <p className="font-bold">€{(item.price * item.count).toFixed(2)}</p>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <button className='px-2 bg-gray-500 text-white font-light rounded mr-1' onClick={(e) => subtractItem(e, item.id)}>-</button>
+                                                <button className='px-2 bg-gray-500 text-white font-light rounded' onClick={(e) => addItem(e, item.id)}>+</button>
                                             </div>
                                         </div>
-                                        <div>
-                                            <button className='px-2 bg-gray-500 text-white font-light rounded mr-1' onClick={(e) => subtractItem(e, item.id)}>-</button>
-                                            <button className='px-2 bg-gray-500 text-white font-light rounded' onClick={(e) => addItem(e, item.id)}>+</button>
-                                        </div>
-                                    </div>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-                <p className='mb-3 font-bold'>Totaal: €{Math.abs(total).toFixed(2)}</p>
-                <button className="px-8 py-1 bg-red-500 rounded text-white w-full disabled:opacity-50" disabled={(cartItems.length <= 0 ? true : false)} onClick={(e) => orderButton(e)}>Bestellen €{Math.abs(total).toFixed(2)}</button>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                    <p className='mb-3 font-bold'>Totaal: €{Math.abs(total).toFixed(2)}</p>
+                    <button className="px-8 py-1 bg-red-500 rounded text-white w-full disabled:opacity-50" disabled={(cartItems.length <= 0 ? true : false)} onClick={(e) => orderButton(e)}>Bestellen €{Math.abs(total).toFixed(2)}</button>
+                </div>
             </div>
         </>
     )
