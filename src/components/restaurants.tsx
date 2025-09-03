a36246a4f7ecc011173def4070f3f20489f05d7f
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-09-03 12:09:58 +0200

    Adds restaurants file

---
+++ src/components/restaurants.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useContext, useEffect } from 'react'
+
+import Link from 'next/link'
+
+import { RestaurantContext } from '@/context/restaurant-context'
+
+import { restaurants } from '../data'
+
+import Restaurant from './restaurant'
+
+function Restaurants() {
+    const [restaurantItems, setRestaurantItems] = useContext(RestaurantContext);
+
+    useEffect(() => {
+    }, [restaurantItems])
+
+
+    return (
+        <>
+            <div className='xl:w-[890px]'>
+                <h3 className='font-bold mb-4 text-2xl'>Bestel bij 4 locaties</h3>
+                <ul className='grid gap-2 items-start'>
+
+                    {
+                        restaurantItems.length === 0 ? (
+                            restaurants.map((item) => {
+                                return <li key={item.id} className='group'>
+                                    <Link href={`/restaurants/${item.id}`}>
+                                        <Restaurant {...item} />
+                                    </Link>
+                                </li>
+                            }
+                            )) : (
+                            restaurantItems.map((item) => {
+                                return  <li key={item.id} className='group'>
+                                    <Link href={`/restaurants/${item.id}`}>
+                                        <Restaurant {...item} />
+                                    </Link>
+                                </li>
+                            })
+                        )
+                    }
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Restaurants
