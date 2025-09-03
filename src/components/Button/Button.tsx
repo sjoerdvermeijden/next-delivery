a97c9b9fcace9d795bb88ed8ceb5e5cfd5564892
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-09-03 09:57:53 +0200

    Removes unused button component

--- src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {
-    children: React.ReactNode;
-}
-
-export const Button = () => {
-    return <button className='bg-red-500 px-2 py-1 rounded-md text-white'>Check</button>
-}
