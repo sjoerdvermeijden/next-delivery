0c28cc4b6a3683fc17f238021e9bd83b25912a71
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-09-03 10:40:39 +0200

    Adds a facebook icon and changes duplicate id

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -20,22 +20,19 @@ function RegisterForm({ }: Props) {
                   </svg>
                   Doorgaan met Google
                 </Button>
-                <Button variant="outline" className="relative w-full rounded-3xl">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='absolute left-2 top-2'>
-                    <path
-                        d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
-                        fill="currentColor"
-                    />
+                <Button variant="outline" className="relative w-full rounded-3xl bg-blue-500 text-white">
+                    <svg viewBox="0 0 24 24" fill="currentColor" height="24px" width="24px" xmlns="http://www.w3.org/2000/svg" className='absolute left-2 top-2'>
+                        <path d="M17 2H14C12.6739 2 11.4021 2.52678 10.4645 3.46447C9.52678 4.40215 9 5.67392 9 7V10H6V14H9V22H13V14H16L17 10H13V7C13 6.73478 13.1054 6.48043 13.2929 6.29289C13.4804 6.10536 13.7348 6 14 6H17V2Z" />
                     </svg>
-                    Doorgaan met Apple
+                    Doorgaan met Facebook
                 </Button>
             </div>
             <div className='relative mb-4'>
                 <p className='flex justify-center w-full before:content-[attr(after)] before:ml-2 before:h-[0.1px] before:bg-gray-200 before:z-10 before:w-[165px] before:absolute before:left-0 before:top-2.5 after:content-[attr(after)] after:ml-2 after:h-[0.1px] after:bg-gray-200 after:z-10 after:w-[165px] after:absolute after:right-0 after:top-2.5 text-gray-600 font-light text-sm'>Of</p>
             </div>
             <form action="" className='flex flex-col mb-4'>
-                <label htmlFor="email" className='mb-1 text-xs font-bold'>Naam</label>
-                <input className='mb-3 border p-2 rounded-xl' type="text" name="" id="email" />
+                <label htmlFor="name" className='mb-1 text-xs font-bold'>Naam</label>
+                <input className='mb-3 border p-2 rounded-xl' type="text" name="" id="name" />
                 <label htmlFor="email" className='mb-1 text-xs font-bold'>E-mailadres</label>
                 <input className='mb-3 border p-2 rounded-xl' type="email" name="" id="email" />
                 <label htmlFor="wachtwoord" className='mb-1 text-xs font-bold'>Wachtwoord</label>
