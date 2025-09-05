3b869bfb860f6de35c44a0a194b87df366c11858
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-09-05 10:55:17 +0200

    Adds another small amount of changes to the menu items dialog close button

--- src/components/menu-item.tsx
+++ src/components/menu-item.tsx
@@ -98,7 +98,7 @@ function MenuItem({ id, title, description, price, image }: Props) {
             <DialogContent hideCloseButton className='fixed top-0 right-0 bottom-0 left-0 w-full translate-y-0 translate-x-0 bg-white border-0 p-5 md:-mt-20 md:top-[50%] md:right-[50%] md:left-[50%] md:bottom-[auto] md:-translate-y-[50%] md:-translate-x-[50%] md:rounded-2xl md:w-[455px] md:max-w-[455px]'>
                 <DialogClose asChild>
                     <div className='absolute right-8 top-8 w-8 h-8 rounded-[50%] bg-gray-100'>
-                        <button type="button" className='relative w-8 h-8 transition-colors  rounded-md hover:bg-gray-100'>
+                        <button type="button" className='relative w-8 h-8'>
                             <span className='inline-block absolute right-2 top-4 w-4 h-0.5 bg-gray-900 rotate-[45deg]'></span>
                             <span className='inline-block absolute right-2 top-4 w-4 h-0.5 bg-gray-900 -rotate-[45deg]'></span>
                         </button>
