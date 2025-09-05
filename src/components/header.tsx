49b8ff1ffc9b7a2b62892bbe1545bc93dcbc7bd9
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-09-05 10:40:19 +0200

    Adds custom close icon for the header dialog

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -43,18 +43,26 @@ function Header({ }: Props) {
                         <DialogTrigger>
                             <IconMenu2 stroke={2} />
                         </DialogTrigger>
-                        <DialogContent className='fixed top-0 right-0 bottom-0 left-0 w-full translate-y-0 translate-x-0 bg-white border-0 p-5 md:-mt-20 md:top-[45%] md:right-[50%] md:left-[50%] md:bottom-[auto] md:-translate-y-[50%] md:-translate-x-[50%] md:rounded-2xl md:w-[600px] md:max-w-[600px]'>
-                            <DialogHeader>
-                                <DialogTitle className='text-2xl font-bold mb-4'>Mijn account</DialogTitle>
-                                    <div className='flex'>
-                                        <DialogClose asChild>
-                                            <Link href="/account" className='text-center mr-4 text-lg text-black font-bold bg-gray-100 p-2 rounded-3xl w-[50%]'>Inloggen</Link>
-                                        </DialogClose>
-                                        <DialogClose asChild>
-                                            <Link href="/create-account" className='text-center text-lg font-bold bg-orange-500 text-white p-2 rounded-3xl w-[50%]'>Registreren</Link>
-                                        </DialogClose>
-                                    </div>
-                            </DialogHeader>
+                        <DialogContent hideCloseButton className='fixed top-0 right-0 bottom-0 left-0 w-full translate-y-0 translate-x-0 bg-white border-0 p-5 md:-mt-20 md:top-[45%] md:right-[50%] md:left-[50%] md:bottom-[auto] md:-translate-y-[50%] md:-translate-x-[50%] md:rounded-2xl md:w-[600px] md:max-w-[600px]'>
+                            <div className='flex items-center mb-4'>
+                                <DialogHeader className='mr-auto'>
+                                    <DialogTitle className='text-2xl font-bold'>Mijn account</DialogTitle>
+                                </DialogHeader>
+                                <DialogClose asChild>
+                                    <button type="button" className='relative w-8 h-8 transition-colors  rounded-md hover:bg-gray-100'>
+                                        <span className='inline-block absolute right-2 top-4 w-4 h-0.5 bg-gray-900 rotate-[45deg]'></span>
+                                        <span className='inline-block absolute right-2 top-4 w-4 h-0.5 bg-gray-900 -rotate-[45deg]'></span>
+                                    </button>
+                                </DialogClose>
+                            </div>
+                            <div className='flex'>
+                                <DialogClose asChild>
+                                    <Link href="/account" className='text-center mr-4 text-lg text-black font-bold bg-gray-100 p-2 rounded-3xl w-[50%]'>Inloggen</Link>
+                                </DialogClose>
+                                <DialogClose asChild>
+                                    <Link href="/create-account" className='text-center text-lg font-bold bg-orange-500 text-white p-2 rounded-3xl w-[50%]'>Registreren</Link>
+                                </DialogClose>
+                            </div>
                             <div className="flex flex-col gap-0">
                                 <ul className='border-b pb-2 mb-2'>
                                     <li className='flex items-center px-4 py-3 text-gray-600 font-light text-sm hover:bg-gray-100 hover:cursor-pointer'>
