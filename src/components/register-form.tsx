651527a87239538b70643f3f732b0331d4b95b28
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-09-17 13:33:14 +0200

    Adds some basic stuff using react hook form

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,24 +1,35 @@
+'use client'
+
 import React from 'react'
+
+import { useForm, SubmitHandler } from "react-hook-form"
 
 import { Button } from "@/components/ui/button"
 
 import Link from 'next/link'
 
-type Props = {}
+interface IFormInput {
+    name: string
+    email: string
+    password: string
+}
 
-function RegisterForm({ }: Props) {
+function RegisterForm({ }) {
+    const { register, handleSubmit } = useForm<IFormInput>()
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+
     return (
         <>
             <h3 className='text-2xl font-bold mb-2'>Registreren</h3>
             <div className='mb-4'>
                 <Button variant="outline" className="relative w-full rounded-3xl mb-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='absolute left-2 top-2'>
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  Doorgaan met Google
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='absolute left-2 top-2'>
+                        <path
+                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                            fill="currentColor"
+                        />
+                    </svg>
+                    Doorgaan met Google
                 </Button>
                 <Button variant="outline" className="relative w-full rounded-3xl bg-blue-500 text-white">
                     <svg viewBox="0 0 24 24" fill="currentColor" height="24px" width="24px" xmlns="http://www.w3.org/2000/svg" className='absolute left-2 top-2'>
@@ -32,13 +43,19 @@ function RegisterForm({ }: Props) {
                 <p className='flex justify-center text-gray-600 font-light text-sm mr-4'>Of</p>
                 <span className='inline-flex w-full h-1.5 border-t border-t-gray-200 mt-1'></span>
             </div>
-            <form action="" className='flex flex-col mb-6'>
+            <form action="" className='flex flex-col mb-6' onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="name" className='mb-1.5 text-xs font-bold'>Naam</label>
-                <input className='mb-3 border p-2 rounded-xl placeholder:text-sm' placeholder="Naam" type="text" name="" id="name" />
+                <input className='mb-3 border p-2 rounded-xl placeholder:text-sm' placeholder="Naam" type="text" id="name" {...register("name", { required: true })} />
                 <label htmlFor="email" className='mb-1.5 text-xs font-bold'>E-mailadres</label>
-                <input className='mb-3 border p-2 rounded-xl placeholder:text-sm' placeholder="Email" type="email" name="" id="email" />
+                <input className='mb-3 border p-2 rounded-xl placeholder:text-sm' placeholder="Email" type="email" id="email" {...register("email", {
+                    required: "required",
+                    pattern: {
+                        value: /\S+@\S+\.\S+/,
+                        message: "Entered value does not match email format",
+                    },
+                })} />
                 <label htmlFor="wachtwoord" className='mb-1.5 text-xs font-bold'>Wachtwoord</label>
-                <input className='mb-4 border p-2 rounded-xl placeholder:text-sm' placeholder="Wachtwoord" type="password" name="" id="wachtwoord" />
+                <input className='mb-4 border p-2 rounded-xl placeholder:text-sm' placeholder="Wachtwoord" type="password" id="wachtwoord" {...register("password", { required: true })}  />
                 <input className='bg-orange-500 p-2 text-white font-bold rounded-3xl' type="submit" value="Registreren" />
             </form>
             <div className='flex justify-center text-xs mb-6'>
